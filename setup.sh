1034e12d73e4a48abb3a31f4bc485d763bd602f4
radiocab <radiocab@mail.ru>
2025-04-01 02:11:57 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,8 @@ if [ -z ${mydomain+x} ]; then echo "mydomain is unset and will be set to test123
 # Navigate to your NGINX configuration directory on your server:
 cd /etc/nginx
 # Create a backup of your current NGINX configuration:
-tar --exclude='nginx*.tar.gz'  -czvf nginx_$(date +'%F_%H-%M-%S').tar.gz nginx.conf sites-available/ sites-enabled/ nginxconfig.io/ conf.d/ modules-available/ modules-enabled/ snippets/
-tar --exclude='nginx*.tar.gz'  -czvf nginx_all_$(date +'%F_%H-%M-%S').tar.gz /etc/nginx/
+tar --exclude='nginx*.tar.gz'  -czf nginx_$(date +'%F_%H-%M-%S').tar.gz nginx.conf sites-available/ sites-enabled/ nginxconfig.io/ conf.d/ modules-available/ modules-enabled/ snippets/
+tar --exclude='nginx*.tar.gz'  -czf nginx_all_$(date +'%F_%H-%M-%S').tar.gz /etc/nginx/
 
 
 curl -o nginx-opencart-setup.zip -fSL "https://github.com/radiocab/nginx-opencart-setup/archive/refs/heads/main.zip"  
