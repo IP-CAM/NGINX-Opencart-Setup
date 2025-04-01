6a1451fd95b1185aa8c006a5d4b609b5272db17a
radiocab <radiocab@mail.ru>
2025-04-01 16:21:04 +0300

    inder-constraction page

--- check-conf.sh
+++ check-conf.sh
@@ -16,4 +16,10 @@ fi
 printf "\n\n\n"
 # if out=$(sudo nginx -t 2>&1); then echo "Configuration was successful" | sudo systemctl reload nginx; else echo "Configuration failure, because: $out"; fi
 
+mkdir /var/www/$mydomain
+
+rm -rf certbot.sh ssl-init.sh setup.sh check-conf.sh
+
+mv ./under-construction.html /var/www/$mydomain/index.html
+
 printf "${OK} Check-conf script ended without errors${NC}"
