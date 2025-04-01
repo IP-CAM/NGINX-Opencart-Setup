b800f37152002c4864163ef89feebd3c3fb42440
radiocab <radiocab@mail.ru>
2025-04-01 14:13:52 +0300

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -8,7 +8,7 @@ echo "Total number of arguments is $#"
 # accordingly to https://www.digitalocean.com/community/tools/nginx?global.security.securityTxt=true&global.logging.errorLogEnabled=true&global.logging.logNotFound=true
 
 echo "${INFO}Commenting out SSL related directives in the configuration${NC}"
-sed -i -r 's/(listen .*443)/\1; #/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g; s/(server \{)/\1\n    ssl off;/g' /etc/nginx/sites-available/example.com.conf
+sed -i -r 's/(listen .*443)/\1; #/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#aaa\1/g; s/(server \{)/\1\n    ssl off;/g' /etc/nginx/sites-available/example.com.conf
 cat /etc/nginx/sites-available/example.com.conf
 # The above command will add a temporary ssl off directive to ensure 
 # that SSL directives are not active. 
@@ -33,8 +33,7 @@ echo "${INFO}Reloading your NGINX server${NC}"
 
 sudo nginx -t && sudo systemctl reload nginx
 
-# Configure Certbot to reload NGINX when it successfully renews certificates:
-
+printf "${INFO}Configure Certbot to reload NGINX when it successfully renews certificates${NC}"
 echo -e '#!/bin/bash\nnginx -t && systemctl reload nginx' | sudo tee /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
 
 sudo chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
