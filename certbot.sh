66e372e0cbb1e121ab0c972da6522194255bd632
radiocab <radiocab@mail.ru>
2025-04-01 13:49:24 +0300

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -9,18 +9,19 @@ echo "Total number of arguments is $#"
 
 echo "${INFO}Commenting out SSL related directives in the configuration${NC}"
 sed -i -r 's/(listen .*443)/\1; #/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g; s/(server \{)/\1\n    ssl off;/g' /etc/nginx/sites-available/example.com.conf
-
+cat /etc/nginx/sites-available/example.com.conf
 # The above command will add a temporary ssl off directive to ensure 
 # that SSL directives are not active. 
 # This may cause NGINX to emit a warning, which is safe to ignore. 
 # The directive will be removed once Certbot is configured.
 
-echo "${INFO}Starting your NGINX server to be able issue certificate (need to see http://example.com/.well-known/acme-challenge/)${NC}"
+echo "${INFO}Starting your NGINX server to be able issue certificate"
+echo "(need to see http://example.com/.well-known/acme-challenge/  to validate domain)${NC}"
 sudo nginx -t && sudo systemctl reload nginx
 
 sudo snap install --classic certbot
 
-echo "${INFO}Obtaining SSL certificates from Let's Encrypt using Certbot${NC}"
+echo "\n${INFO}Obtaining SSL certificates from Let's Encrypt using Certbot${NC}"
 certbot certonly --webroot -d example.com --email info@example.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal 
 # --pre-hook "service nginx stop" --post-hook "service nginx start"
 
