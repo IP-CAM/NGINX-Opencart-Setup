9e79c8e65dafbbe08fd95c855d8ff3044c6dec3d
radiocab <radiocab@mail.ru>
2025-04-02 21:44:59 +0300

    Update install-opencart.sh

--- install-opencart.sh
+++ install-opencart.sh
@@ -5,19 +5,20 @@ set -e
 mydomain=$1    # e.g.reallymydomain.site
 if [ -z ${mydomain+x} ] || [ "$mydomain" = "reallymydomain.site" ] ; then 
  printf "${ERR}You need to set YOUR own domain as first argument. Exiting..${NC}" && exit 1 
-else 
-  printf "${OK}Domain is set to '$mydomain'\n${NC}\n"
 fi
-
+printf "${OK}Domain is set to '$mydomain'\n${NC}\n"
+ 
 releaseurl=$2  # e.g. 'https://github.com/opencart/opencart/releases/download/3.0.3.2/opencart-3.0.3.2.zip'
 echo $releaseurl
 if  [ -z ${releaseurl+x} ] ; then releaseurl='https://github.com/opencart/opencart/releases/download/3.0.4.0/opencart-3.0.4.0.zip'; fi
+printf "${OK}Release url is set to '$releaseurl'\n${NC}\n"
 
 releaseroot=$3 # e.g. 'upload-3040' 
 if  [ -z ${releaseroot+x} ] ; then releaseroot='upload'; fi
+printf "${OK}Release root is set to '$releaseroot'\n${NC}\n"
 
 webroot=/var/www/$mydomain/public
-echo $webroot
+printf "${OK}Web root is  '$webroot'\n${NC}\n"
 
 
 ################################
