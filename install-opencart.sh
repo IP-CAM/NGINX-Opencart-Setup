4240c5d0a8da32b4dbba2989626ac513b790377f
radiocab <radiocab@mail.ru>
2025-04-02 21:23:32 +0300

    Update install-opencart.sh

--- install-opencart.sh
+++ install-opencart.sh
@@ -6,11 +6,11 @@ mydomain=$1    # e.g.reallymydomain.site
 if [ -z ${mydomain+x} ] || [ "$mydomain" = "reallymydomain.site" ] ; then 
  printf "${ERR}You need to set YOUR own domain as first argument. Exiting..${NC}" && exit 1 
 else 
-  printf "${OK}Domain is set to '$mydomain'${NC}\n"
+  printf "${OK}Domain is set to '$mydomain'\n${NC}\n"
 fi
 
 releaseurl=$2  # e.g. 'https://github.com/opencart/opencart/releases/download/3.0.3.2/opencart-3.0.3.2.zip'
-if  [ -z ${releaseurl+x} ] ; then 'https://github.com/opencart/opencart/releases/download/3.0.4.0/opencart-3.0.4.0.zip'; fi
+if  [ -z ${releaseurl+x} ] ; then releaseurl='https://github.com/opencart/opencart/releases/download/3.0.4.0/opencart-3.0.4.0.zip'; fi
 
 releaseroot=$3 # e.g. 'upload-3040' 
 if  [ -z ${releaseroot+x} ] ; then releaseroot='upload'; fi
@@ -22,7 +22,7 @@ webroot='/var/www/$mydomain/public'
 #      Install Opencart        #
 ################################
 
-mkdir tmp && cd tmp
+mkdir -p tmp && cd tmp
 curl -o oc.zip -fSL $releaseurl
 unzip oc.zip
 rm oc.zip;
