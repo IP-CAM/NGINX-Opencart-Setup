d516dbcd4b3bafac4bec1db967674d45164c8438
radiocab <radiocab@mail.ru>
2025-04-05 21:23:13 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,19 +2,10 @@
 
 set -e 
 
-randomtmp=tmp-"$(pwgen -1 -s 5)"
-mkdir -p $randomtmp && cd $randomtmp
-curl -o oc-setup.zip -fSL https://github.com/radiocab/nginx-opencart-setup/archive/refs/heads/main.zip
-unzip -o -q oc-setup.zip
-rm oc-setup.zip;
-(cd ./nginx-opencart-setup-main && tar c .) | (cd ./ && tar xf -)
-
-source ./includes.sh
+source ./includes.sh 
 
 setcolors
 read_args_by_name
 source ./$ACTION.sh
-footermsg 
 
-cd ..
-rm -r $randomtmp
+footermsg 
