881c7c2988c3d2efcd80b46df2f209be2a6d7c9e
radiocab <radiocab@mail.ru>
2025-04-06 13:48:45 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,21 +6,13 @@ source ./includes.sh
 set_colors
 read_args_by_name
 
-# set -e has already been set so no need to reverse it after the command:
 if [[ $- =~ e ]]; then
     # set -e has already been set so we need to set it back it after the command
     set +e
-    source ./$action1.sh
+    source ./$action.sh
 	footermsg 
     set -e	
 else
-    source ./$action1.sh
+    source ./$action.sh
     footermsg 
 fi
-#source ./$action1.sh
-
-# set -e has already been set so no need to reverse it after the command
-# if [[ $- =~ e ]]; then ....    
-# source ./$action1.sh
-
-
