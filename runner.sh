1d0595db046d705dcfb8c215c4306c0b0e4eec42
radiocab <radiocab@mail.ru>
2025-04-06 14:16:53 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -4,7 +4,7 @@ scriptname=runner.sh
 source ./includes.sh "$@"
 
 #set_colors
-read_args_by_name "$@"
+read_args_by_name 
 echo "\n# arguments runner called with ---->  ${@}\n"
 if [[ $- =~ e ]]; then
     # set -e has already been set so we need to set it back it after the command
