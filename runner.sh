6cd95bcd7bf6fda2e3cf5dd8e86d1f5d2ccbcba3
radiocab <radiocab@mail.ru>
2025-04-06 13:15:11 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-set -e 
 scriptname=runner.sh
 source ./includes.sh 
 
 set_colors
 read_args_by_name
-source ./$action1.sh
+
+if [[ $- =~ e ]]; then
+    # set -e has already been set so no need to reverse it after the command
+    source ./$action1.sh
+else
+    set -e
+    source ./$action1.sh
+    set +e
+fi
+
+# set -e has already been set so no need to reverse it after the command
+# if [[ $- =~ e ]]; then ....    
+# source ./$action1.sh
 
 footermsg 
 echo "Exiting '$scriptname' with code:$?  "$?
