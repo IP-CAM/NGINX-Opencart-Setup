c849b4c9171176f618b2867ed23e1ae7ef81e3ca
radiocab <radiocab@mail.ru>
2025-04-06 18:31:14 +0300

    Update setup-menu.sh

--- setup-menu.sh
+++ setup-menu.sh
@@ -3,115 +3,8 @@
  
 trap "exit 1" TERM
 export TOP_PID=$$ 
- 
- 
-# thanks to https://askubuntu.com/posts/1386907/revisions
-simple_choose_from_menu() {
-    local prompt="$1" outvar="$2"
-    shift
-    shift
-    local options=("$@") cur=0 count=${#options[@]} index=0
-    local esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
-    printf "$prompt\n"
-    while true
-    do
-        # list all options (option list is zero-based)
-        index=0 
-        for o in "${options[@]}"
-        do
-            if [ "$index" == "$cur" ]
-            then echo -e " >\e[7m$o\e[0m" # mark & highlight the current option
-            else echo "  $o"
-            fi
-            (( index++ ))
-        done
-        read -s -n3 key # wait for user to key in arrows or ENTER
-        if [[ $key == $esc[A ]] # up arrow
-        then (( cur-- )); (( cur < 0 )) && (( cur = 0 ))
-        elif [[ $key == $esc[B ]] # down arrow
-        then (( cur++ )); (( cur >= count )) && (( cur = count - 1 ))
-        elif [[ $key == "" ]] # nothing, i.e the read delimiter - ENTER
-        then break
-        fi
-        echo -en "\e[${count}A" # go up to the beginning to re-render
-    done
-    # export the selection to the requested output variable
-    printf -v $outvar "${options[$cur]}"
- }
- 
-
-simple_menu() {
-selections=(
- "Selection A"
- "Selection B"
- "Selection C"
- )
-
- simple_choose_from_menu "Please make a choice:" selected_choice "${selections[@]}"
- echo "Selected choice: $selected_choice"
-}
-# thanks to https://askubuntu.com/posts/563226/revisions
-# https://web.archive.org/web/20180130222805/http://pro-toolz.net/data/programming/bash/Bash_fancy_menu.html
-# only works with bash
-fancy_menu() {
-     E='echo -e';e='echo -en';trap "R;exit" 2
-    ESC=$( $e "\e")
-   TPUT(){ $e "\e[${1};${2}H";}
-  CLEAR(){ $e "\ec";}
-  CIVIS(){ $e "\e[?25l";}
-   DRAW(){ $e "\e%@\e(0";}
-  WRITE(){ $e "\e(B";}
-   MARK(){ $e "\e[7m";}
- UNMARK(){ $e "\e[27m";}
-      R(){ CLEAR ;stty sane;$e "\ec\e[37;44m\e[J";};
-   HEAD(){ DRAW
-           for each in $(seq 1 13);do
-           $E "   x                                          x"
-           done
-           WRITE;MARK;TPUT 1 5
-           $E "BASH SELECTION MENU                       ";UNMARK;}
-           i=0; CLEAR; CIVIS;NULL=/dev/null
-   FOOT(){ MARK;TPUT 13 5
-           printf "ENTER - SELECT,NEXT                       ";UNMARK;}
-  ARROW(){ read -s -n3 key 2>/dev/null >&2
-           if [[ $key = $ESC[A ]];then echo up;fi
-           if [[ $key = $ESC[B ]];then echo dn;fi;}
-     M0(){ TPUT  4 20; $e "Login info";}
-     M1(){ TPUT  5 20; $e "Network";}
-     M2(){ TPUT  6 20; $e "Disk";}
-     M3(){ TPUT  7 20; $e "Routing";}
-     M4(){ TPUT  8 20; $e "Time";}
-     M5(){ TPUT  9 20; $e "ABOUT  ";}
-     M6(){ TPUT 10 20; $e "EXIT   ";}
-      LM=6
-   MENU(){ for each in $(seq 0 $LM);do M${each};done;}
-    POS(){ if [[ $cur == up ]];then ((i--));fi
-           if [[ $cur == dn ]];then ((i++));fi
-           if [[ $i -lt 0   ]];then i=$LM;fi
-           if [[ $i -gt $LM ]];then i=0;fi;}
-REFRESH(){ after=$((i+1)); before=$((i-1))
-           if [[ $before -lt 0  ]];then before=$LM;fi
-           if [[ $after -gt $LM ]];then after=0;fi
-           if [[ $j -lt $i      ]];then UNMARK;M$before;else UNMARK;M$after;fi
-           if [[ $after -eq 0 ]] || [ $before -eq $LM ];then
-           UNMARK; M$before; M$after;fi;j=$i;UNMARK;M$before;M$after;}
-   INIT(){ R;HEAD;FOOT;MENU;}
-     SC(){ REFRESH;MARK;$S;$b;cur=`ARROW`;}
-     ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
-  while [[ "$O" != " " ]]; do case $i in
-        0) S=M0;SC;if [[ $cur == "" ]];then R;$e "\n$(w        )\n";ES;fi;;
-        1) S=M1;SC;if [[ $cur == "" ]];then R;$e "\n$(ifconfig )\n";ES;fi;;
-        2) S=M2;SC;if [[ $cur == "" ]];then R;$e "\n$(df -h    )\n";ES;fi;;
-        3) S=M3;SC;if [[ $cur == "" ]];then R;$e "\n$(route -n )\n";ES;fi;;
-        4) S=M4;SC;if [[ $cur == "" ]];then R;$e "\n$(date     )\n";ES;fi;;
-        5) S=M5;SC;if [[ $cur == "" ]];then R;$e "\n$($e by oTo)\n";ES;fi;;
-        6) S=M6;SC;if [[ $cur == "" ]];then R;exit 0;fi;;
-  esac;POS;done
-}
 
 #https://askubuntu.com/questions/1705/how-can-i-create-a-select-menu-in-a-shell-script
-
-
 
 oc_options_menu() {
 
@@ -182,24 +75,8 @@ dialog                         --begin 2 2 --yesno "" 0 0 \
 					--output-separator '-------string2-----' \
     --and-widget               --begin 6 6  "" 0 0
 }	
-#Only the last widget is left visible:
-menu2() { 
-dialog           --clear       --begin 2 2 --yesno "" 0 0 \
-    --and-widget --clear       --begin 4 4 --yesno "" 0 0 \
-    --and-widget               --begin 6 6 --yesno "" 0 0
-}
-# All three widgets visible, staircase effect, ordered 3,2,1:
-menu3() {
-dialog           --keep-window --begin 2 2 --yesno "" 0 0 \
-    --and-widget --keep-window --begin 4 4 --yesno "" 0 0 \
-    --and-widget               --begin 6 6 --yesno "" 0 0
-}	
-# First and third widget visible, staircase effect, ordered 3,1:
-menu4() {
-dialog           --keep-window --begin 2 2 --yesno "" 0 0 \
-    --and-widget --clear       --begin 4 4 --yesno "" 0 0 \
-    --and-widget               --begin 6 6 --yesno "" 0 0
-}
+ 
+ 
 
 # Note that dialog is not universally available on all Linux systems(thought on Ubuntu is available)
 # Script might not be compatible across different systems/releases/distributions. 
@@ -213,8 +90,8 @@ if [[ $? != 0 ]]; then
   simple_menu
 fi  
 menu1
-fancy_menu
-simple_menu
+
+
 
 sudo apt-get -qq install zenity
 selection=$(zenity --list "Option 1" "Option 2" "Option 3" --column="" --text="Text above column(s)" --title="My menu")
@@ -225,9 +102,7 @@ case "$selection" in
 "Option 3")zenity --info --text="Do something here for No3";;
 esac
 
-#menu2
-#menu3
-#menu4
+ 
 #oc_options_menu
 
 scripturl='https://raw.githubusercontent.com/radiocab/nginx-opencart-setup/refs/heads/main/bootstrap-runner.sh'
