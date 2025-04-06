572a69db5b8cc859ad345decf83d9b04f4c23b73
radiocab <radiocab@mail.ru>
2025-04-06 13:34:41 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -8,12 +8,12 @@ read_args_by_name
 
 # set -e has already been set so no need to reverse it after the command:
 if [[ $- =~ e ]]; then
-    # set -e has already been set so no need to reverse it after the command
-    source ./$action1.sh
-else
+    # set -e has already been set so we need to set it back it after the command
     set +e
     source ./$action1.sh
-    set -e
+    set -e	
+else
+     source ./$action1.sh
 fi
 #source ./$action1.sh
 
