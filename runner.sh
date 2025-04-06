0f3b29a7cb47192c20d9b0501d12f09a61333ef1
radiocab <radiocab@mail.ru>
2025-04-06 13:51:41 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -5,7 +5,7 @@ source ./includes.sh
 
 set_colors
 read_args_by_name
-
+echo "# arguments called with ---->  ${@}"
 if [[ $- =~ e ]]; then
     # set -e has already been set so we need to set it back it after the command
     set +e
