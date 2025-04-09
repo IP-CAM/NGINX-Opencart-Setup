98125c652254fb5a5e47138fa058ff80633986d5
radiocab <radiocab@mail.ru>
2025-04-09 15:38:23 +0300

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -14,23 +14,23 @@ printf "${INFO}Commenting out SSL related directives in the configuration
  that SSL directives are not active. ${NC}"
 printf "${WARN}This may cause NGINX to emit several warnings below, which are safe to ignore.${NC}"
 printf "${INFO}The directive 'ssl off;' will be removed once Certbot is configured.${NC}"
-sed -i -r 's/(listen .*443)/\1; #/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g; s/(server \{)/\1\n    ssl off;/g' /etc/nginx/sites-available/example.com.conf
-# cat /etc/nginx/sites-available/example.com.conf
+sed -i -r 's/(listen .*443)/\1; #/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g; s/(server \{)/\1\n    ssl off;/g' /etc/nginx/sites-available/$mydomain.conf
+# cat /etc/nginx/sites-available/$mydomain.com.conf
 
 
 printf "${INFO}Starting your NGINX server to be able issue certificate
- (need to see http://example.com/.well-known/acme-challenge/  to validate domain)${NC}"
+ (need to see http://$mydomain/.well-known/acme-challenge/  to validate domain)${NC}"
 sudo nginx -t && sudo systemctl stop nginx && sudo systemctl start nginx
 
 sudo snap install --classic certbot
 # ‐‐dry‐run
 if  [ "$dry_run" = "--dry-run" ] ; then 
  printf "${INFO}Dry-run for certbot SSL certificates from Let's Encrypt using Certbot${NC}"
- certbot certonly --webroot -d example.com --email info@$mydomain -w /var/www/_letsencrypt -n --agree-tos --force-renewal --dry-run
+ certbot certonly --webroot -d $mydomain --email info@$mydomain -w /var/www/_letsencrypt -n --agree-tos --force-renewal --dry-run
  mkdir -p  /etc/letsencrypt/live/$mydomain
 else
  printf "${INFO}Obtaining SSL certificates from Let's Encrypt using Certbot${NC}"
- certbot certonly --webroot -d example.com --email info@$mydomain -w /var/www/_letsencrypt -n --agree-tos --force-renewal 
+ certbot certonly --webroot -d $mydomain --email info@$mydomain -w /var/www/_letsencrypt -n --agree-tos --force-renewal 
 # --pre-hook "service nginx stop" --post-hook "service nginx start"
 fi
  
